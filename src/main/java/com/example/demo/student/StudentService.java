121e79604e71efc63557f466075923d640b28484
SoLetsDev <derek.so@gov.bc.ca>
2021-11-23 08:46:36 -0800

    added psql functionality with docker container

---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,14 @@
+package com.example.demo.student;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+  public List<Student> getStudents() {
+    return List.of(
+        new Student(1L, "mark", "mark@gmail.com", LocalDate.of(2000, Month.APRIL, 5), 21));
+  }
+}
